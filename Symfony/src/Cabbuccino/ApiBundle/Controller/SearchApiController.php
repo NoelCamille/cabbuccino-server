37770287da6bad163d7f6aaec5861241e79158ec
NoelCamille <bmwb.noel@gmail.com>
2013-03-04 15:19:31 +0100

    minor changes

--- Symfony/src/Cabbuccino/ApiBundle/Controller/SearchApiController.php
+++ Symfony/src/Cabbuccino/ApiBundle/Controller/SearchApiController.php
@@ -3,6 +3,8 @@
 namespace Cabbuccino\ApiBundle\Controller;
 
 
+use Cabbuccino\UserBundle\Entity\Travel;
+use Cabbuccino\UserBundle\Entity\Subscription;
 use Symfony\Component\HttpFoundation\Response as Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -10,29 +12,109 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class SearchApiController extends ApiController
 {
 	/**
-     * @Route("/search")
+     * @Route("/search/id={userId}/dep={dep}/arr={arr}")
      * @Template()
      */
 
-    public function searchAction()
+    public function searchAction($dep,$arr,$userId)
     {
+        $postedTravel = $this->searchTravel($dep,$arr);
+        
+        if (sizeof($postedTravel)==0){
+        	$travel = $this->createTravel($dep,$arr);
+        	$subscription = $this->createSubscription($travel->getId(),$userId);
+        }
+        
+        else{
+        	$travel = $postedTravel[0];
+        	$subscription = $this->createSubscription($travel,$this->searchUser($userId));
+        }
+        
+        return new Response(json_encode($travel->asMobileObject()));
+    }
     
-        $em = $this->getDoctrine()->getEntityManager();
+    public function createTravel($dep,$arr){
+  		$travel = new Travel();
+  		$travel->setDeparture($dep);
+  		$travel->setArrival($arr);
+  		
+  		$em = $this->getDoctrine()->getManager();
+    	$em->persist($travel);
+    	$em->flush();
+    	
+    	return $travel;
+  		
+  	}
+  	
+  	public function createSubscription($travel,$user){
+  		$subscription = new Subscription();
+  		$subscription->setTravelId($travel);
+  		$subscription->setUserId($user);
+  		
+  		$em = $this->getDoctrine()->getManager();
+    	$em->persist($subscription);
+    	$em->flush();
+    	
+    	return $subscription;
+  		
+  	}
+  
+    public function searchTravel($departure,$arrival){
+    	
+    	$em = $this->getDoctrine()->getEntityManager();
         
         $qb = $em->createQueryBuilder();
         $qb ->add('select','u')
             ->add('from','CabbuccinoUserBundle:Travel u')
             ->add('where','u.departure=:dep AND u.arrival=:arr');
         
-        $qb->setParameter('dep',11);
-        $qb->setParameter('arr',12);
-            
+        $qb->setParameter('dep',$departure);
+        $qb->setParameter('arr',$arrival);
+        
         $query = $qb->getQuery();
-        $travel = $query->getResult();
+        $travels = $query->getResult();
         
-        $this->setResponseData($travel);    
-        return $this->renderResponse();
-        
+        return $this->availableTravel($travels);
     }
-  
+    
+    public function availableTravel($travels){
+
+    	$em = $this->getDoctrine()->getEntityManager();
+  	
+    	$qb = $em->createQueryBuilder();
+        $qb ->add('select','s')
+            ->add('from','CabbuccinoUserBundle:Subscription s')
+            ->add('where','s.travel_id=:travelId');
+    	
+    	$availableTravel=array();
+    	
+    	for ($i=0; $i<sizeof($travels) ; $i++){
+    		$travel = $travels[$i];
+    		$qb->setParameter('travelId',$travel->getId());
+    		$query = $qb->getQuery();
+    		$subscriptionsLinkedToTravel= $query->getResult();
+    		if (sizeOf($subscriptionsLinkedToTravel)<3){
+    			$availableTravel[sizeOf($availableTravel)] = $travel;
+    		}
+    	}
+    	
+    	return $availableTravel;
+    }
+    
+    public function searchUser($id){
+    	$em = $this->getDoctrine()->getEntityManager();
+        
+        $qb = $em->createQueryBuilder();
+        $qb ->add('select','u')
+            ->add('from','CabbuccinoUserBundle:User u')
+            ->add('where','u.id=:id');
+        
+        $qb->setParameter('id',$id);
+        $query = $qb->getQuery();
+        $user = $query->getSingleResult();
+        
+        return $user;
+    }
+    
+    
 }
